a3140c368119844ea5403d03512ccfb54dff1cc9
bvvivek6 <vivekbv242813@gmail.com>
2025-02-22 21:38:00 +0530

    Add Initialize Tip Calculator React app with essential files, components, and styles

---
+++ tipcalculator/src/App.js
@@ -0,0 +1,94 @@
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
+
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [perc1, setPerc1] = useState(0);
+  const [perc2, setPerc2] = useState(0);
+
+  const tip = (bill * ((perc1 + perc2) / 2 / 100)).toFixed(2);
+
+  function reset() {
+    setBill("");
+  }
+
+  return (
+    <div>
+      <BillInput billAmt={bill} onSetBill={setBill} />
+      <Selecpercentage percentage={perc1} onSelect={setPerc1}>
+        How was the Experience?
+      </Selecpercentage>
+      <Selecpercentage percenetage={perc2} onSelect={setPerc2}>
+        What did your friend think about the experience?
+      </Selecpercentage>
+      <Output billAmt={bill} tip={tip} />
+      <Reset reset={reset} />
+    </div>
+  );
+}
+
+function BillInput({ billAmt, onSetBill }) {
+  return (
+    <div>
+      <label>How Much was the Bill?</label>
+      <input
+        type="text"
+        placeholder="Enter amount..."
+        value={billAmt}
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      ></input>
+    </div>
+  );
+}
+
+function Selecpercentage({ children, percentage, onSelect }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select
+        value={percentage}
+        onChange={(e) => onSelect(Number(e.target.value))}
+      >
+        <option value="0">Dissatisfied 0%</option>
+        <option value="5">It was Okay 5%</option>
+        <option value="10">It was Good 10%</option>
+        <option value="20">It was Amazing 10%</option>
+      </select>
+    </div>
+  );
+}
+
+function Output({ billAmt, tip }) {
+  return (
+    <div>
+      <h3 style={{ fontWeight: "800" }}>
+        You Pay ${billAmt + tip} (${billAmt} + ${tip})
+      </h3>
+    </div>
+  );
+}
+
+function Reset({ reset }) {
+  return (
+    <di>
+      <button
+        onClick={reset}
+        style={{
+          padding: "5px",
+          width: "100px",
+          fontWeight: "600",
+          fontSize: "1rem",
+        }}
+      >
+        Reset
+      </button>
+    </di>
+  );
+}
